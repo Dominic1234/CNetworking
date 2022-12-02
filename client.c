8df497aa3bb34058c92ad089a6ec168aa2340cdc
Dominic1234 <dhruv.sharma.017@gmail.com>
2022-12-02 23:52:22 +1100

    added error handling and multi-client support

--- client.c
+++ client.c
@@ -17,15 +17,28 @@ int main(int argc, char *argv[]) {
 
 	mysocket = socket(AF_INET, SOCK_STREAM, 0);
 
-	memset(&dest, 0, sizeof(dest));
-	dest.sin_family = AF_INET;
-	dest.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-	dest.sin_port = htons(PORTNUM);
+	if(mysocket < 0) {
+		printf("Error in connection\n");
+		exit(1);
+	}
+	printf("Client socket is created.\n");
 
-	connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr_in));
+	memset(&dest, 0, sizeof(dest));			/* zero the struct */
+	dest.sin_family = AF_INET;
+	dest.sin_addr.s_addr = htonl(INADDR_LOOPBACK);	/* set destination IP number - localhost, 127.0.0.1 */
+	dest.sin_port = htons(PORTNUM);			/* set destination port number */
+
+	res = connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr_in));
+
+	if(ret < 0) {
+		printf("Error in connection\n");
+		exit(1);
+	}
+	printf("Connected to Server\n");
 
 	len = recv(mysocket, buffer, MAXRCVLEN, 0);
 
+	/* We have to null terminate the received data ourselves */
 	buffer[len] = '\0';
 
 	printf("Received %s (%d bytes). \n", buffer, len);
--- server.c
+++ server.c
@@ -12,22 +12,43 @@
 int main(int argc, char *argv[]) {
 	char* msg = "Hello, World\n";
 
-	struct sockaddr_in dest;
-	struct sockaddr_in serv;
-	int mysocket;
+	struct sockaddr_in dest;			/* socket info about client */
+	struct sockaddr_in serv;			/* socket info about server */
+	int mysocket;					/* socket used to listen for incoming connections */
 	socklen_t socksize = sizeof(struct sockaddr_in);
 
-	memset(&serv, 0, sizeof(serv));
-	serv.sin_family = AF_INET;
-	serv.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv.sin_port = htons(PORTNUM);
+	memset(&serv, 0, sizeof(serv));			/* empty struct before filling fields */
+	serv.sin_family = AF_INET;			/* set connection type to TCP/IP */
+	serv.sin_addr.s_addr = htonl(INADDR_ANY);	/* set server address to any interface */
+	serv.sin_port = htons(PORTNUM);			/* set server port number */
 
 	mysocket = socket(AF_INET, SOCK_STREAM, 0);
 
-	bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr));
+	if(mysocket < 0) {
+		printf("Error in connection\n");
+		exit(1);
+	}
 
-	listen(mysocket, 1);
+	printf("Server socket created\n");
+
+	/* bind server info to mysocket*/
+	int ret = bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr));
+
+	if(ret < 0) {
+		printf("Ërror in binding\n");
+		exit(1);
+	}
+
+	/* start listening, allowing a queue of 1 pending connection */
+	if(listen(mysocket, 10) == 0) {
+		printf("Listening...\n\n");
+	}
+
 	int consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
+
+	if(consocket < ) {
+		exit(1);
+	}
 
 	while(consocket) {
 		printf("Incoming connection from %s - sending welcome\n", inet_ntoa(dest.sin_addr));
