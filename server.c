65e1090ae8e77ef8e880198423217692ae19396d
Dominic1234 <dhruv.sharma.017@gmail.com>
2022-12-03 23:59:51 +1100

    Making changes for multi-threading

--- server.c
+++ server.c
@@ -6,16 +6,39 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 #define PORTNUM 2300
+
+// Semaphore variables
+sem_t x, y;
+pthread_t tid;
+pthread_t threads[100];
+int count = 0;
+
+void* writer(void* param) {
+	printf("Writer entering\n");
+	// Lock semaphore
+	sem_wait(&y);
+	printf("Writer entered\n");
+	// Unlock semaphore
+	sem_post(&y);
+	printf("Writer leaving\n");
+}
 
 int main(int argc, char *argv[]) {
 	char* msg = "Hello, World\n";
 
 	struct sockaddr_in dest;			/* socket info about client */
 	struct sockaddr_in serv;			/* socket info about server */
+	struct sockaddr_storage serverStorage;
+
+	socklen_t addr_size;
+	sem_init(&x, 0, 1);
+	sem_init(&y, 0, 1);
+
 	int mysocket;					/* socket used to listen for incoming connections */
-	socklen_t socksize = sizeof(struct sockaddr_in);
 	pid_t childpid;
 
 	memset(&serv, 0, sizeof(serv));			/* empty struct before filling fields */
@@ -41,23 +64,27 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* start listening, allowing a queue of 1 pending connection */
-	if(listen(mysocket, 10) == 0) {
+	if(listen(mysocket, 50) == 0) {
 		printf("Listening...\n\n");
 	}
-
-	int consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
-
-	if(consocket < 0) {
+	else {
+		printf("Error\n");
 		exit(1);
 	}
 
-	while(consocket) {
-		printf("Incoming connection from %s - sending welcome\n", inet_ntoa(dest.sin_addr));
-		send(consocket, msg, strlen(msg), 0);
+	pthread_t tid[60];
+
+	int consocket;
+
+	while(1) {
+		addr_size = sizeof(serverStorage);
 		consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
+		if(consocket < 0) {
+			printf("Error\n");
+			exit(1);
+		}
 	}
 
-	close(consocket);
 	close(mysocket);
 	return EXIT_SUCCESS;
 }
