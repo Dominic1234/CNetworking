41603c70b0f50cd23229d35c1ae9089f60e347dd
Deepak Sharma <sharmadpk@gmail.com>
2022-12-17 22:01:10 +0530

    Added notes

--- test.sh
+++ test.sh
@@ -3,12 +3,16 @@
 		# bring server to the foreground. Pressing Ctrl-C will impact server. Connected clients will also be killed
 		#		First command into background is %1
 
+		echo ">>> Starting server in the background"
 		./server &
 		sleep 1		# Allow server to open server socket
+		echo ">>> Starting clients in parallel"
 		./client &
 		./client &
-		fg %1
-		echo server exit value $?
-		sleep 5
-		killall server	# Just to be sure, leaving no baggage
+		echo ">>> Waiting for clients to complete"
+		jobs
+		# sleep 5	# Allow all programs to complete
+		wait %2 %3
+		echo ">>> Killing server and clients"
+		killall server	client # Just to be sure, leaving no baggage
 
